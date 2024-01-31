d75124de693478151ee5fff060dc5b6cd8a015c8
Dalet <104555136+Spod101@users.noreply.github.com>
2024-01-31 22:45:09 +0800

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -60,7 +60,7 @@ const wave4 = document.querySelector(".wave4");
 
 
 buttonL.addEventListener("click", () => {
-    body.style.backgroundImage = "url('./Styles/luzonbg.webp')";
+    body.style.backgroundImage = "url('./Styles/luzonBG.webp')";
     body.style.backgroundSize = "cover";
     buttonL.style.filter = "blur(0)";
     buttonL.style.filter = "grayscale(0)";
